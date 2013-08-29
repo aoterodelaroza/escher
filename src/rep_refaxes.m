6752b1838a99ed877404808a2641ca0a698868f9
Alberto Otero de la Roza <aoterodelaroza@ucmerced.edu>
2013-08-29 13:12:59 -0700

    modified rep_refaxes: thicker sticks and no negative axes

--- src/rep_refaxes.m
+++ src/rep_refaxes.m
@@ -10,8 +10,8 @@
 % FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
 % more details.
 
-function rep = rep_refaxes(addto="",scale=1,rad=0.01)
-% function rep = rep_refaxes(addto="",scale=1,rad=0.01)
+function rep = rep_refaxes(addto="",scale=1,rad=0.03)
+% function rep = rep_refaxes(addto="",scale=1,rad=0.03)
 %
 % rep_refaxes - add reference axes using 3 sticks (x=red, y=green, z=blue)
 %               with length scale.
@@ -49,13 +49,13 @@ function rep = rep_refaxes(addto="",scale=1,rad=0.01)
   rep.stick{n}.r = rad;
   rep.stick{n}.rgb = [255 0 0 0 0];
   rep.stick{n}.tex = "stick_default";
-  n++;
-  rep.stick{n}.name = "x-axis (negative)";
-  rep.stick{n}.x0 = xct + [0 0 0];
-  rep.stick{n}.x1 = xct - [1 0 0] * scale;
-  rep.stick{n}.r = rad;
-  rep.stick{n}.rgb = [28 0 0 0 0];
-  rep.stick{n}.tex = "stick_default";
+  # n++;
+  # rep.stick{n}.name = "x-axis (negative)";
+  # rep.stick{n}.x0 = xct + [0 0 0];
+  # rep.stick{n}.x1 = xct - [1 0 0] * scale;
+  # rep.stick{n}.r = rad;
+  # rep.stick{n}.rgb = [28 0 0 0 0];
+  # rep.stick{n}.tex = "stick_default";
   ## y axis
   n++;
   rep.stick{n}.name = "y-axis";
@@ -64,13 +64,13 @@ function rep = rep_refaxes(addto="",scale=1,rad=0.01)
   rep.stick{n}.r = rad;
   rep.stick{n}.rgb = [0 255 0 0 0];
   rep.stick{n}.tex = "stick_default";
-  n++;
-  rep.stick{n}.name = "y-axis (negative)";
-  rep.stick{n}.x0 = xct + [0 0 0];
-  rep.stick{n}.x1 = xct - [0 1 0] * scale;
-  rep.stick{n}.r = rad;
-  rep.stick{n}.rgb = [0 28 0 0 0];
-  rep.stick{n}.tex = "stick_default";
+  # n++;
+  # rep.stick{n}.name = "y-axis (negative)";
+  # rep.stick{n}.x0 = xct + [0 0 0];
+  # rep.stick{n}.x1 = xct - [0 1 0] * scale;
+  # rep.stick{n}.r = rad;
+  # rep.stick{n}.rgb = [0 28 0 0 0];
+  # rep.stick{n}.tex = "stick_default";
   ## z axis
   n++;
   rep.stick{n}.name = "z-axis";
@@ -79,13 +79,13 @@ function rep = rep_refaxes(addto="",scale=1,rad=0.01)
   rep.stick{n}.r = rad;
   rep.stick{n}.rgb = [0 0 255 0 0];
   rep.stick{n}.tex = "stick_default";
-  n++;
-  rep.stick{n}.name = "z-axis (negative)";
-  rep.stick{n}.x0 = xct + [0 0 0];
-  rep.stick{n}.x1 = xct - [0 0 1] * scale;
-  rep.stick{n}.r = rad;
-  rep.stick{n}.rgb = [0 0 28 0 0];
-  rep.stick{n}.tex = "stick_default";
+  # n++;
+  # rep.stick{n}.name = "z-axis (negative)";
+  # rep.stick{n}.x0 = xct + [0 0 0];
+  # rep.stick{n}.x1 = xct - [0 0 1] * scale;
+  # rep.stick{n}.r = rad;
+  # rep.stick{n}.rgb = [0 0 28 0 0];
+  # rep.stick{n}.tex = "stick_default";
   rep.nstick = n;
 
 endfunction
