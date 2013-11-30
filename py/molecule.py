d69c3249a4716526d75f7111961dfcd07ebbebba
Daniel Menendez <danielmail7@gmail.com>
2013-11-30 21:51:11 +0100

    Reads xyz data. VTK installation intructions.
    Only single structure xyz file by now is supported.
    py/escher.txt contains the steps that need to be followed
    ind order to be able to use the pythonic escher subdivision

--- py/molecule.py
+++ py/molecule.py
@@ -11,15 +11,19 @@
 # more details.
 
 #from __future__ import print_function, division
-#from math import cos, sin, pi, sqrt, acos, degrees
 from math import sqrt
 import shlex as lex
+from ConfigParser import SafeConfigParser
 import numpy as np
-#import pybel
 from chem import elements 
 import logcolors
 from representation import Representation
 from grid import Grid
+
+parser = SafeConfigParser()
+parser.read('escher.ini')
+
+print parser.get('molecule', 'structfile')
 
 log = logcolors.escherlog()
 #log.setLevel(log.DEBUG)
@@ -29,7 +33,8 @@ class Molecule():
     '''
     molecule data structure
     >>> mol = Molecule()
-    >>> mol.readcube('../../escher_data/mol/ethylene_iso/c2h4.cube')
+    >>> mol.structfile = '../../escher_data/mol/ethylene_iso/c2h4.cube'
+    >>> mol.readstruct()
     >>> mol.a
     array([[ 13.33332 ,   0.      ,   0.      ],
            [  0.      ,  13.33332 ,   0.      ],
@@ -67,7 +72,7 @@ class Molecule():
         self.rep.window()
 
 
-    def abrir(self, filename):
+    def openf(self, filename):
         """
         Opens a file and returns lines without whitespaces.
         """
@@ -78,28 +83,54 @@ class Molecule():
         lines = map(lambda s: s.strip(), lines)
         return lines
 
-    def readcube(self, filename):
+    def readstruct(self):
+
+        if self.structfile.endswith('.cube'):
+            self.readcube()
+        if self.structfile.endswith('.xyz'):
+            self.readxyz()
+
+    def readxyz(self):
+        """
+        Extracts structure information 
+        from a *.xyz file
+        """
+
+        log.debug('Reading file {}'.format(self.structfile))
+
+        ## parsing is not done the clever way, it could be improved
+        lines = self.openf(self.structfile)
+        for n,i in enumerate(lines):
+            if n==0: # first line
+                self.nat = int(lex.split(i)[0])
+                log.debug('Number of atoms: {}'.format(self.nat))
+                # initialize atomic names, coordinates with right size
+                self.atxyz = np.zeros([self.nat,3])
+            elif n==1: # second line
+                self.title = i
+                log.debug('Title: {}'.format(self.title))
+            elif n==2:
+                for j in range(self.nat):
+                    self.atname = np.append(self.atname, 
+                            [str(lex.split(lines[n+j])[0])])
+                    #self.atname.append(elements.find(self.atnumber[-1]))
+                    #self.atmass = np.append(self.atmass, 
+                    #        [float(lex.split(lines[n+j])[1])])
+                    self.atxyz[j,:] = np.array( \
+                            [float(k) for k in lex.split(lines[n+j])[1:4]])
+                log.debug('Atoms: \n {}'.format(self.atname))
+                log.debug('Atomic coordinates: \n {}'.format(self.atxyz))
+
+    def readcube(self):
         """
         Extracts structure information and grid values
         from a Gaussian *.cube file
         """
 
-        ## Openbabel data 
-        #mol = pybel.readfile("cube", filename).next()
-        #file = open(filename, 'r')
-        #self.name = file.readline()
-        #self.title = file.readline()
-        #self.nat = file.readline()[0]
-        #self.nat = len(mol.atoms)
-        #for atom in mol:
-            #self.atnumber.append(atom.atomicnum)
-            #self.atname.append(elements.find(atom.atomicnum))
-            #self.atmass.append(atom.atomicmass)
-            #self.atxyz.append(atom.coords)
-        log.debug('Reading file {}'.format(filename))
+        log.debug('Reading file {}'.format(self.structfile))
 
         ## parsing is not done the clever way, it could be improved
-        lines = self.abrir(filename)
+        lines = self.openf(self.structfile)
         for n,i in enumerate(lines):
             if n==0: # first line
                 self.name = i
@@ -124,19 +155,176 @@ class Molecule():
 
     def stickball(self):
         log.debug('Add stick/ball representation')
+        # Jmol colors
+        colors = { \
+         'H'  : (255,255,255)  ,
+         'He' : (217,255,255)  ,
+         'Li' : (204,128,255)  ,
+         'Be' : (194,255,0)    ,
+         'B'  : (255,181,181)  ,
+         'C'  : (144,144,144)  ,
+         'N'  : (48,80,248)    ,
+         'O'  : (255,13,13)    ,
+         'F'  : (144,224,80)   ,
+         'Ne' : (179,227,245)  ,
+         'Na' : (171,92,242)   ,
+         'Mg' : (138,255,0)    ,
+         'Al' : (191,166,166)  ,
+         'Si' : (240,200,160)  ,
+         'P'  : (255,128,0)    ,
+         'S'  : (255,255,48)   ,
+         'Cl' : (31,240,31)    ,
+         'Ar' : (128,209,227)  ,
+         'K'  : (143,64,212)   ,
+         'Ca' : (61,255,0)     ,
+         'Sc' : (230,230,230)  ,
+         'Ti' : (191,194,199)  ,
+         'V'  : (166,166,171)  ,
+         'Cr' : (138,153,199)  ,
+         'Mn' : (156,122,199)  ,
+         'Fe' : (224,102,51)   ,
+         'Co' : (240,144,160)  ,
+         'Ni' : (80,208,80)    ,
+         'Cu' : (200,128,51)   ,
+         'Zn' : (125,128,176)  ,
+         'Ga' : (194,143,143)  ,
+         'Ge' : (102,143,143)  ,
+         'As' : (189,128,227)  ,
+         'Se' : (255,161,0)    ,
+         'Br' : (166,41,41)    ,
+         'Kr' : (92,184,209)   ,
+         'Rb' : (112,46,176)   ,
+         'Sr' : (0,255,0)      ,
+         'Y'  : (148,255,255)  ,
+         'Zr' : (148,224,224)  ,
+         'Nb' : (115,194,201)  ,
+         'Mo' : (84,181,181)   ,
+         'Tc' : (59,158,158)   ,
+         'Ru' : (36,143,143)   ,
+         'Rh' : (10,125,140)   ,
+         'Pd' : (0,105,133)    ,
+         'Ag' : (192,192,192)  ,
+         'Cd' : (255,217,143)  ,
+         'In' : (166,117,115)  ,
+         'Sn' : (102,128,128)  ,
+         'Sb' : (158,99,181)   ,
+         'Te' : (212,122,0)    ,
+         'I'  : (148,0,148)    ,
+         'Xe' : (66,158,176)   ,
+         'Cs' : (87,23,143)    ,
+         'Ba' : (0,201,0)      ,
+         'La' : (112,212,255)  ,
+         'Ce' : (255,255,199)  ,
+         'Pr' : (217,255,199)  ,
+         'Nd' : (199,255,199)  ,
+         'Pm' : (163,255,199)  ,
+         'Sm' : (143,255,199)  ,
+         'Eu' : (97,255,199)   ,
+         'Gd' : (69,255,199)   ,
+         'Tb' : (48,255,199)   ,
+         'Dy' : (31,255,199)   ,
+         'Ho' : (0,255,156)    ,
+         'Er' : (0,230,117)    ,
+         'Tm' : (0,212,82)     ,
+         'Yb' : (0,191,56)     ,
+         'Lu' : (0,171,36)     ,
+         'Hf' : (77,194,255)   ,
+         'Ta' : (77,166,255)   ,
+         'W'  : (33,148,214)   ,
+         'Re' : (38,125,171)   ,
+         'Os' : (38,102,150)   ,
+         'Ir' : (23,84,135)    ,
+         'Pt' : (208,208,224)  ,
+         'Au' : (255,209,35)   ,
+         'Hg' : (184,184,208)  ,
+         'Tl' : (166,84,77)    ,
+         'Pb' : (87,89,97)     ,
+         'Bi' : (158,79,181)   ,
+         'Po' : (171,92,0)     ,
+         'At' : (117,79,69)    ,
+         'Rn' : (66,130,150)   ,
+         'Fr' : (66,0,102)     ,
+         'Ra' : (0,125,0)      ,
+         'Ac' : (112,171,250)  ,
+         'Th' : (0,186,255)    ,
+         'Pa' : (0,161,255)    ,
+         'U'  : (0,143,255)    ,
+         'Np' : (0,128,255)    ,
+         'Pu' : (0,107,255)    ,
+         'Am' : (84,92,242)    ,
+         'Cm' : (120,92,227)   ,
+         'Bk' : (138,79,227)   ,
+         'Cf' : (161,54,212)   ,
+         'Es' : (179,31,212)   ,
+         'Fm' : (179,31,186)   ,
+         'Md' : (179,13,166)   ,
+         'No' : (189,13,135)   ,
+         'Lr' : (199,0,102)    ,
+         'Rf' : (204,0,89)     ,
+         'Db' : (209,0,79)     ,
+         'Sg' : (217,0,69)     ,
+         'Bh' : (224,0,56)     ,
+         'Hs' : (230,0,46)     ,
+         'Mt' : (235,0,38)     \
+        }
+        rcov = { \
+        'H'  : 37 ,        'Nb' :    137,   'Tl' : 148,
+        'He' : 32 ,        'Mo' :    145,   'Pb' : 147,
+        'Li' : 134,        'Tc' :    156,   'Bi' : 146,
+        'Be' : 90 ,        'Ru' :    126,   'Po' : 200,
+        'B'  : 82 ,        'Rh' :    135,   'At' : 200,
+        'C'  : 77 ,        'Pd' :    131,   'Rn' : 145,
+        'N'  : 75 ,        'Ag' :    153,   'Fr' : 200,
+        'O'  : 73 ,        'Cd' :    148,   'Ra' : 200,
+        'F'  : 71 ,        'In' :    144,   'Ac' : 200,
+        'Ne' : 69 ,        'Sn' :    141,   'Th' : 200,
+        'Na' : 154,        'Sb' :    138,   'Pa' : 200,
+        'Mg' : 130,        'Te' :    135,   'U'  : 200,
+        'Al' : 118,        'I'  :    133,   'Np' : 200,
+        'Si' : 111,        'Xe' :    130,   'Pu' : 200,
+        'P'  : 106,        'Cs' :    225,   'Am' : 200,
+        'S'  : 102,        'Ba' :    198,   'Cm' : 200,
+        'Cl' : 99 ,        'La' :    169,   'Bk' : 200,
+        'Ar' : 97 ,        'Ce' :    200,   'Cf' : 200,
+        'K'  : 196,        'Pr' :    200,   'Es' : 200,
+        'Ca' : 174,        'Nd' :    200,   'Fm' : 200,
+        'Sc' : 144,        'Pm' :    200,   'Md' : 200,
+        'Ti' : 136,        'Sm' :    200,   'No' : 200,
+        'V'  : 125,        'Eu' :    200,   'Lr' : 200,
+        'Cr' : 127,        'Gd' :    200,   'Rf' : 200,
+        'Mn' : 139,        'Tb' :    200,   'Db' : 200,
+        'Fe' : 125,        'Dy' :    200,   'Sg' : 200,
+        'Co' : 126,        'Ho' :    200,   'Bh' : 200,
+        'Ni' : 121,        'Er' :    200,   'Hs' : 200,
+        'Cu' : 138,        'Tm' :    200,   'Mt' : 200,
+        'Zn' : 131,        'Yb' :    200,          
+        'Ga' : 126,        'Lu' :    160,          
+        'Ge' : 122,        'Hf' :    150,          
+        'As' : 119,        'Ta' :    138,          
+        'Se' : 116,        'W'  :    146,          
+        'Br' : 114,        'Re' :    159,          
+        'Kr' : 110,        'Os' :    128,          
+        'Rb' : 211,        'Ir' :    137,          
+        'Sr' : 192,        'Pt' :    128,          
+        'Y'  : 162,        'Au' :    144,
+        'Zr' : 148,        'Hg' :    149,
+        }
+
         for i in range(self.nat):
             # instead use a dictionary
-            if self.atname[i] == 'H':
-                color = [1,1,1]
-            if self.atname[i] == 'C':
-                color = [0.5,0.5,0.5]
-            if self.atname[i] == 'N':
-                color = [0,0,1,255]
-            if self.atname[i] == 'O':
-                color = [1,0,0]
-            else:
-                color = [0.5,0.5,0.5]
-            sphereatom = self.rep.ball(0.6, self.atxyz[i], color)
+            #if self.atname[i] == 'H':
+            #    color = [1,1,1]
+            #elif self.atname[i] == 'C':
+            #    color = [0.5,0.5,0.5]
+            #elif self.atname[i] == 'N':
+            #    color = [0,0,1,255]
+            #elif self.atname[i] == 'O':
+            #    color = [1,0,0]
+            #else:
+            #    color = [1.0,0.5,0.0]
+            color = [ col/255. for col in colors[self.atname[i]]]
+            radi = rcov[self.atname[i]]/200.
+            sphereatom = self.rep.ball(radi, self.atxyz[i], color)
             self.rep.ren.AddActor(sphereatom)
 
         for i in range(self.nat):
@@ -144,29 +332,42 @@ class Molecule():
                 dist = sqrt((self.atxyz[i][0] - self.atxyz[j][0])**2 + 
                             (self.atxyz[i][1] - self.atxyz[j][1])**2 + 
                             (self.atxyz[i][2] - self.atxyz[j][2])**2)
-                if (dist) < 3. :
+                if (dist) < ((rcov[self.atname[i]]+rcov[self.atname[j]])/60.) :
                     stickbond = self.rep.stick(self.atxyz[i], 
                                                self.atxyz[j])
                     self.rep.ren.AddActor(stickbond)
 
-    def isosurface(self, filename, isovalue):
-        dens = Grid()
-        #dens.readcube('../../escher_data/mol/ethylene_iso/c2h4.cube')
-        dens.readcube(filename)
-        delta = [dens.dx[0,0], dens.dx[1,1], dens.dx[2,2]]
-        self.rep.gridvolume(dens.x0, delta, dens.n, dens.f)
-        self.rep.marchingcubes(isovalue)
-        
-    def nciplot(self, file1, file2):
-        dens = Grid()
-        grad = Grid()
+    def isosurface(self, filename):
 
-        dens.readcube(file1)
-        grad.readcube(file2)
-        delta = [grad.dx[0,0], grad.dx[1,1], grad.dx[2,2]]
-        self.rep.gridvolume(grad.x0, delta, grad.n, grad.f)
-        self.rep.volumescalar(dens.n, dens.f)
-        self.rep.marchingcubes(0.5)
+        grid = Grid()
+
+        grid.readcube(filename)
+        delta = [grid.dx[0,0], grid.dx[1,1], grid.dx[2,2]]
+
+        self.rep.imagedata(grid.x0, delta, grid.n)
+        self.rep.outline()
+        gridarray = self.rep.array(grid.n, grid.f, 'scalars')
+        self.rep.grid.GetPointData().SetScalars(gridarray)
+        self.rep.marchingcubes(self.isovalue)
+        
+    def nciplot(self):
+
+        self.isovalue = 0.5
+        self.rep.lookuptable()
+        self.isosurface(self.gradfile)
+
+        dens = Grid()
+        dens.readcube(self.densfile)
+        densarray = self.rep.array(dens.n, dens.f, 'dens')
+        self.rep.grid.GetPointData().AddArray(densarray)
+
+        self.rep.mapper.ScalarVisibilityOn()
+        self.rep.mapper.SetLookupTable(self.rep.colorNCI)
+        self.rep.mapper.SetScalarModeToUsePointFieldData()
+        self.rep.mapper.SelectColorArray(1)
+        self.rep.mapper.SetScalarRange(-5.0, 5.0)
+        self.rep.scalarbar()
+        
 
     def start(self):
 
--- py/representation.py
+++ py/representation.py
@@ -10,7 +10,6 @@
 # FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
 # more details.
 
-#from math import cos, sin, pi, sqrt, acos, degrees
 from math import sqrt, acos, degrees
 import vtk
 import numpy as np
@@ -33,10 +32,7 @@ class Representation():
         self.name = ""
 
         self.nball = 0
-        #self.ball = []
-
         self.nstick = 0
-        #self.stick = []
 
         self.ntriangle = 0
         self.nvertex = 0
@@ -177,7 +173,133 @@ class Representation():
         #png.SetFileName('img.png')
         #png.Write()
     
-    def gridvolume(self, orig, delta, dims, scalars):
+    def imagedata(self, orig, delta, dims):
+
+        log.debug('Scalar grid volume data')
+        # construct the volume
+        grid = vtk.vtkImageData()
+        grid.SetOrigin(orig) 
+        grid.SetSpacing(delta)
+        grid.SetDimensions(dims) # number of points in each direction
+
+        self.grid = grid
+
+    def array(self, dims, scalars, name):
+
+        log.debug('vtkDoubleArray creation')
+        array = vtk.vtkDoubleArray()
+        array.SetNumberOfComponents(1) # this is 3 for a vector
+        array.SetNumberOfTuples(self.grid.GetNumberOfPoints())
+
+        nd = 0
+        for k in range(dims[2]):
+            for j in range(dims[1]):
+                for i in range(dims[0]):
+                    array.SetValue(nd, scalars[i,j,k])
+                    nd = nd + 1
+
+        array.SetName(name)
+
+        return array
+
+    def outline(self):
+
+        bounds = vtk.vtkOutlineFilter()
+        bounds.SetInputData(self.grid)
+
+        boundsMapper = vtk.vtkPolyDataMapper()
+        boundsMapper.SetInputConnection(bounds.GetOutputPort())
+
+        boundsActor = vtk.vtkActor()
+        boundsActor.SetMapper(boundsMapper)
+        boundsActor.GetProperty().SetColor(0, 0, 0)
+        self.ren.AddActor(boundsActor)
+
+    def lookuptable(self):
+
+        #lutNCI = vtk.vtkLookupTable()
+        #lutNCI.SetNumberOfColors(3)
+        #lutNCI.SetHueRange(0.667, 0.0)
+        #lutNCI.SetSaturationRange(1.0, 1.0)
+        #lutNCI.SetValueRange(0.8, 0.8)
+        #lutNCI.Build()
+
+        # color palette
+        # -3.0 -> blue
+        #  0.1 -> green
+        #  3.0 -> red
+        colorNCI = vtk.vtkColorTransferFunction()
+        colorNCI.AddRGBPoint(-3.0,0.0,0.0,1.0)
+        colorNCI.AddRGBPoint(0.1,0.0,1.0,0.0)
+        colorNCI.AddRGBPoint(3.0,1.0,0.0,0.0)
+        self.colorNCI = colorNCI
+
+    def scalarbar(self):
+
+        scalarBar = vtk.vtkScalarBarActor()
+        scalarBar.SetLookupTable(self.mapper.GetLookupTable())
+        scalarBar.SetTitle("sign(lambda_2)rho")
+        scalarBar.SetOrientationToHorizontal()
+        scalarBar.GetLabelTextProperty().SetColor(0,0,1)
+        scalarBar.GetTitleTextProperty().SetColor(0,0,1)
+        # position it in window
+        coord = scalarBar.GetPositionCoordinate()
+        coord.SetCoordinateSystemToNormalizedViewport()
+        coord.SetValue(0.1,0.05)
+        scalarBar.SetWidth(.8)
+        scalarBar.SetHeight(.085)
+
+        self.ren.AddActor(scalarBar)
+
+
+    def marchingcubes(self, isovalue):
+
+        log.debug('Marching Cubes algorithm contour filter')
+        # ContourFilter or MarchingCubes
+        #isoExtractor = vtk.vtkContourFilter()
+        isoExtractor = vtk.vtkMarchingCubes()
+        isoExtractor.SetInputData(self.grid)
+        # isosurface id and value
+        isoExtractor.SetValue(0, isovalue)
+        #isoExtractor.GenerateValues(4, isovalue-0.05, isovalue+0.05)
+        isoExtractor.ComputeGradientsOn()
+
+        isoNormals = vtk.vtkPolyDataNormals()
+        isoNormals.SetInputConnection(isoExtractor.GetOutputPort())
+        isoNormals.SetFeatureAngle(35.0)
+        isoNormals.ConsistencyOn()
+        isoNormals.SplittingOn()
+
+        isoStripper = vtk.vtkStripper()
+        isoStripper.SetInputConnection(isoNormals.GetOutputPort())
+
+        isoLocator = vtk.vtkCellLocator()
+        isoLocator.SetDataSet(isoExtractor.GetOutput())
+        isoLocator.LazyEvaluationOn()
+
+        isoMapper = vtk.vtkPolyDataMapper()
+        isoMapper.SetInputConnection(isoStripper.GetOutputPort())
+        isoMapper.ScalarVisibilityOn()
+        self.mapper = isoMapper
+        #isoMapper.InterpolateScalarsBeforeMappingOn()
+        #isoMapper.SetScalarModeToUsePointData()
+        #isoMapper.ColorByArrayComponent('dens', 0) # doesn't work
+        #isoMapper.GetArrayName('dens') # doesn't work
+
+        isoProperty = vtk.vtkProperty()
+        isoProperty.SetColor(0.2,1,0.2) # ~green
+        #isoProperty.SetInterpolationToPhong()
+
+        iso = vtk.vtkActor()
+        iso.SetMapper(isoMapper)
+        iso.SetProperty(isoProperty)
+        #iso.GetProperty().SetRepresentationToWireframe()
+        #iso.GetProperty().SetOpacity(.6)
+
+        #self.ren.AddViewProp(iso)
+        self.ren.AddActor(iso)
+
+    def _gridvolume(self, orig, delta, dims, scalars):
 
         log.debug('Scalar grid volume data')
         # construct the volume
@@ -226,7 +348,7 @@ class Representation():
 #        array2.SetName("dens")
 #        self.grid.GetPointData().SetScalars(array2)
 
-    def volumescalar(self, dims, scalars):
+    def _volumescalar(self, dims, scalars):
 
         array = vtk.vtkDoubleArray()
         array.SetNumberOfComponents(1) # this is 3 for a vector
@@ -242,83 +364,6 @@ class Representation():
         array.SetName("dens")
         self.array = array
         self.grid.GetPointData().AddArray(array)
-
-    def marchingcubes(self, isovalue):
-
-        log.debug('Marching Cubes algorithm contour filter')
-        # ContourFilter or MarchingCubes
-        #isoExtractor = vtk.vtkContourFilter()
-        isoExtractor = vtk.vtkMarchingCubes()
-        isoExtractor.SetInputData(self.grid)
-        # isosurface id and value
-        isoExtractor.SetValue(0, isovalue)
-        #isoExtractor.GenerateValues(4, isovalue-0.05, isovalue+0.05)
-        isoExtractor.ComputeGradientsOn()
-
-        isoNormals = vtk.vtkPolyDataNormals()
-        isoNormals.SetInputConnection(isoExtractor.GetOutputPort())
-        isoNormals.SetFeatureAngle(35.0)
-        isoNormals.ConsistencyOn()
-        isoNormals.SplittingOn()
-
-        isoStripper = vtk.vtkStripper()
-        isoStripper.SetInputConnection(isoNormals.GetOutputPort())
-
-        isoLocator = vtk.vtkCellLocator()
-        isoLocator.SetDataSet(isoExtractor.GetOutput())
-        isoLocator.LazyEvaluationOn()
-
-        #lutNCI = vtk.vtkLookupTable()
-        #lutNCI.SetNumberOfColors(3)
-        #lutNCI.SetHueRange(0.667, 0.0)
-        #lutNCI.SetSaturationRange(1.0, 1.0)
-        #lutNCI.SetValueRange(0.8, 0.8)
-        #lutNCI.Build()
-
-        colorNCI = vtk.vtkColorTransferFunction()
-        colorNCI.AddRGBPoint(-3.0,0.0,0.0,1.0)
-        colorNCI.AddRGBPoint(0.1,0.0,1.0,0.0)
-        colorNCI.AddRGBPoint(3.0,1.0,0.0,0.0)
-
-        isoMapper = vtk.vtkPolyDataMapper()
-        isoMapper.SetInputConnection(isoStripper.GetOutputPort())
-        isoMapper.ScalarVisibilityOn()
-        #isoMapper.SetLookupTable(lutNCI)
-        isoMapper.SetLookupTable(colorNCI)
-        isoMapper.SetScalarModeToUsePointFieldData()
-        isoMapper.SelectColorArray(1)
-        isoMapper.SetScalarRange(-5.0, 5.0)
-        #isoMapper.InterpolateScalarsBeforeMappingOn()
-        #isoMapper.SetScalarModeToUsePointData()
-        #isoMapper.ColorByArrayComponent('dens', 0) # doesn't work
-        #isoMapper.GetArrayName('dens') # doesn't work
-
-        scalarBar = vtk.vtkScalarBarActor()
-        scalarBar.SetLookupTable(isoMapper.GetLookupTable())
-        scalarBar.SetTitle("sign(lambda_2)rho")
-        scalarBar.SetOrientationToHorizontal()
-        scalarBar.GetLabelTextProperty().SetColor(0,0,1)
-        scalarBar.GetTitleTextProperty().SetColor(0,0,1)
-        # position it in window
-        coord = scalarBar.GetPositionCoordinate()
-        coord.SetCoordinateSystemToNormalizedViewport()
-        coord.SetValue(0.1,0.05)
-        scalarBar.SetWidth(.8)
-        scalarBar.SetHeight(.085)
-
-        #isoProperty = vtk.vtkProperty()
-        #isoProperty.SetColor(0.2,1,0.2) # ~green
-        #isoProperty.SetInterpolationToPhong()
-
-        iso = vtk.vtkActor()
-        iso.SetMapper(isoMapper)
-        #iso.SetProperty(isoProperty)
-        #iso.GetProperty().SetRepresentationToWireframe()
-        #iso.GetProperty().SetOpacity(.6)
-
-        #self.ren.AddViewProp(iso)
-        self.ren.AddActor(iso)
-        self.ren.AddActor(scalarBar)
 
 
 
