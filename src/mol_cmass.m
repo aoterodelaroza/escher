472b6fa51ffa1dfdb08a4cf9f39528c3836a1f1a
Alberto Otero de la Roza <alberto@carbono.quimica.uniovi.es>
2014-04-16 11:39:21 -0600

    rewritten mol_cmass

--- src/mol_cmass.m
+++ src/mol_cmass.m
@@ -10,51 +10,31 @@
 % FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
 % more details.
 
-function [cm,mass] = mol_cmass(mol, iat=[], LOG=0)
-% function [cm,mass] = mol_cmass(mol, iat=[], LOG=0)
+function [cm,mass] = mol_cmass(mol,noweight=0)
+% function [cm,mass] = mol_cmass(mol)
 %
-% mol_cmass - determine the center of mass of a molecule or part.
+% mol_cmass - determine the center of mass of a molecule. If noweight=1,
+% make all the atoms weigh the same.
 %
 % Required input variables:
-% {mol}: the molecule.
-%
-% Optional input variables (all have default values):
-% {iat}: list of atoms in the molecule that will be taken into account.
-%     By default (an empty iat) use the whole molecule.
-% {LOG}: print the final result if LOG>0.
+% mol: the molecule.
+% noweight: make all the atomic weights equal to 1. This 
+% is useful when computing the geometric barycenter of the molecule. 
 %
 % Required output variables:
-% {cm}: column vector with the center of mass coordinates.
-% {mass}: total mass of the molecule or fragment.
+% cm: column vector with the center of mass coordinates.
+% mass: total mass of the molecule or fragment.
 %
 % Authors: VLC Victor Lua~na .......... <victor@carbono.quimica.uniovi.es>
 %          AOR Alberto Otero-de-la-Roza <alberto@carbono.quimica.uniovi.es>
 % Created: December 2011
 
-# Check input data, particularly iat1 and iat2.
-if (isempty(iat))
-   iat = 1:mol.nat;
-endif
-
-# Get the center of mass:
-xi = mol.atxyz(1:3,iat); massi = mol.atmass(iat);
-mass = sum(massi);
-cm = xi * mol.atmass(iat)' / mass;
-
-if (LOG > 0)
-   printf('mol_cmass: center of mass of (part of?) a molecule\n');
-   printf('Fragment (%d):', mol.nat);
-   printf(' %d', iat);
-   printf('\n');
-   for i = 1 : length(iat)
-      ii = iat(i);
-      printf('%4d %2s', ii, mol.atname{ii});
-      printf('%12.6f%12.6f%12.6f', mol.atxyz(1:3,ii));
-      printf('\n');
-   endfor
-   printf('\nCenter of mass:');
-   printf('%12.6f%12.6f%12.6f', cm);
-   printf('\n');
-endif
+  if (!noweight)
+    mass = sum(mol.atmass);
+    cm = sum(mol.atxyz * mol.atmass',2) / mass;
+  else
+    mass = 1;
+    cm = sum(mol.atxyz,2) / mol.nat;
+  endif
 
 endfunction
