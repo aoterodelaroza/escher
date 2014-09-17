7366092f88e82ad7fb03d2e98ec2af25db266b84
Alberto Otero de la Roza <aoterodelaroza@ucmerced.edu>
2014-09-17 11:00:50 -0600

    option axcolor in cr_unitcell

--- src/cr_unitcell.m
+++ src/cr_unitcell.m
@@ -10,8 +10,8 @@
 % FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
 % more details.
 
-function rep = cr_unitcell(cr, addto="", i0=[0 0 0], i1=[0 0 0], radius=0.03, rgb=[255 0 0], tex="stick_default", LOG=0)
-% function rep = cr_unitcell(cr, addto="", i0=[0 0 0], i1=[0 0 0], radius=0.05, rgb=[255 0 0], tex="stick_default", LOG=0)
+function rep = cr_unitcell(cr, addto="", i0=[0 0 0], i1=[0 0 0], radius=0.03, rgb=[255 0 0], tex="stick_default", axcolor="")
+% function rep = cr_unitcell(cr, addto="", i0=[0 0 0], i1=[0 0 0], radius=0.05, rgb=[255 0 0], tex="stick_default", axcolor="")
 %
 % cr_unitcell - create sticks for the crystal unit cell.
 %
@@ -32,7 +32,7 @@ function rep = cr_unitcell(cr, addto="", i0=[0 0 0], i1=[0 0 0], radius=0.03, rg
 % tex: string identifier of the stick texture. This is interpreted (in subsequent calls
 %      to the rep routines, i.e., not immediately) as the texture of the stick by calling 
 %      the internal texture database.
-% {LOG}: verbose level (0=silent,1=verbose).
+% axcolor: if 1, plot only the x, y and z axes and use colors for them.
 %
 
   bohr2ang = 0.52917720859;
@@ -52,21 +52,6 @@ function rep = cr_unitcell(cr, addto="", i0=[0 0 0], i1=[0 0 0], radius=0.03, rg
   g = cr.g;
   r *= bohr2ang;
 
-  x0 = [
-        0 0 0 1 0 0
-        0 0 0 0 1 0
-        0 0 0 0 0 1
-        1 0 0 1 1 0
-        1 0 0 1 0 1
-        0 1 0 0 1 1
-        0 1 0 1 1 0
-        0 0 1 1 0 1
-        0 0 1 0 1 1
-        1 1 1 1 1 0
-        1 1 1 1 0 1
-        1 1 1 0 1 1
-        ];
-
   if (isfield(cr,"name"))
     name = strcat(cr.name,"_cell");
   else
@@ -74,22 +59,67 @@ function rep = cr_unitcell(cr, addto="", i0=[0 0 0], i1=[0 0 0], radius=0.03, rg
   endif
 
   [rep itex] = rep_registertexture(rep,tex);
-  for ix = i0(1):i1(1)
-    for iy = i0(2):i1(2)
-      for iz = i0(3):i1(3)
-        for i = 1:size(x0,1)
-          ## add the stick
-          rep.nstick = rep.nstick + 1;
-          rep.stick{rep.nstick} = stick();
-          rep.stick{rep.nstick}.name = name;
-          rep.stick{rep.nstick}.x0 = ([ix iy iz] + x0(i,1:3)) * r;
-          rep.stick{rep.nstick}.x1 = ([ix iy iz] + x0(i,4:6)) * r;
-          rep.stick{rep.nstick}.r = radius;
-          rep.stick{rep.nstick}.rgb = rgb;
-          rep.stick{rep.nstick}.tex = itex;
+
+  if (isempty(axcolor)) 
+    x0 = [
+          0 0 0 1 0 0
+          0 0 0 0 1 0
+          0 0 0 0 0 1
+          1 0 0 1 1 0
+          1 0 0 1 0 1
+          0 1 0 0 1 1
+          0 1 0 1 1 0
+          0 0 1 1 0 1
+          0 0 1 0 1 1
+          1 1 1 1 1 0
+          1 1 1 1 0 1
+          1 1 1 0 1 1
+          ];
+    for ix = i0(1):i1(1)
+      for iy = i0(2):i1(2)
+        for iz = i0(3):i1(3)
+          for i = 1:size(x0,1)
+            ## add the stick
+            rep.nstick = rep.nstick + 1;
+            rep.stick{rep.nstick} = stick();
+            rep.stick{rep.nstick}.name = name;
+            rep.stick{rep.nstick}.x0 = ([ix iy iz] + x0(i,1:3)) * r;
+            rep.stick{rep.nstick}.x1 = ([ix iy iz] + x0(i,4:6)) * r;
+            rep.stick{rep.nstick}.r = radius;
+            rep.stick{rep.nstick}.rgb = rgb;
+            rep.stick{rep.nstick}.tex = itex;
+          end
         end
       end
     end
-  end
+  else
+    ## x
+    rep.nstick = rep.nstick + 1;
+    rep.stick{rep.nstick} = stick();
+    rep.stick{rep.nstick}.name = name;
+    rep.stick{rep.nstick}.x0 = [0 0 0] * r;
+    rep.stick{rep.nstick}.x1 = [1 0 0] * r;
+    rep.stick{rep.nstick}.r = radius;
+    rep.stick{rep.nstick}.rgb = [255 0 0];
+    rep.stick{rep.nstick}.tex = itex;
+    ## y
+    rep.nstick = rep.nstick + 1;
+    rep.stick{rep.nstick} = stick();
+    rep.stick{rep.nstick}.name = name;
+    rep.stick{rep.nstick}.x0 = [0 0 0] * r;
+    rep.stick{rep.nstick}.x1 = [0 1 0] * r;
+    rep.stick{rep.nstick}.r = radius;
+    rep.stick{rep.nstick}.rgb = [0 255 0];
+    rep.stick{rep.nstick}.tex = itex;
+    ## z
+    rep.nstick = rep.nstick + 1;
+    rep.stick{rep.nstick} = stick();
+    rep.stick{rep.nstick}.name = name;
+    rep.stick{rep.nstick}.x0 = [0 0 0] * r;
+    rep.stick{rep.nstick}.x1 = [0 0 1] * r;
+    rep.stick{rep.nstick}.r = radius;
+    rep.stick{rep.nstick}.rgb = [0 0 255];
+    rep.stick{rep.nstick}.tex = itex;
+  endif
 
 endfunction
