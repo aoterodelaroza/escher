f3d6454a30d88f3e5745259dde1529e05d05077f
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2016-06-12 17:15:09 -0300

    reference representation for rep_refaxes

--- src/rep_refaxes.m
+++ src/rep_refaxes.m
@@ -10,14 +10,15 @@
 % FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
 % more details.
 
-function rep = rep_refaxes(addto="",scale=1,srad=1)
-% function rep = rep_refaxes(addto="",scale=1,srad=1)
+function rep = rep_refaxes(addto="",refrep="",scale=1,srad=1)
+% function rep = rep_refaxes(addto="",refrep="",scale=1,srad=1)
 %
 % rep_refaxes - add reference axes using 3 sticks (x=red, y=green, z=blue)
 %               with length scale.
 %
 % Required input variables:
 % addto: input representation.
+% refrep: representation that gives the center of mass for the axes.
 % scale: length of the axes.
 % srad: radius of the axes is srad times 0.05.
 %
@@ -32,7 +33,11 @@ function rep = rep_refaxes(addto="",scale=1,srad=1)
   endif
 
   ## center of mass
-  [xct xmin xmax xdel] = rep_getcm(rep);
+  if (!isempty(refrep) && isstruct(refrep)) 
+      [xct xmin xmax xdel] = rep_getcm(refrep);
+  else
+      xct = [0 0 0];
+  endif
 
   ## add sticks
   n = rep.nstick;
@@ -49,14 +54,7 @@ function rep = rep_refaxes(addto="",scale=1,srad=1)
   rep.stick{n}.r = srad * 0.05;
   rep.stick{n}.rgb = [255 0 0 0 0];
   rep.stick{n}.tex = itex;
-  # n++;
-  # rep.stick{n} = stick();
-  # rep.stick{n}.name = "x-axis (negative)";
-  # rep.stick{n}.x0 = xct + [0 0 0];
-  # rep.stick{n}.x1 = xct - [1 0 0] * scale;
-  # rep.stick{n}.r = srad * 0.05;
-  # rep.stick{n}.rgb = [28 0 0 0 0];
-  # rep.stick{n}.tex = itex;
+
   ## y axis
   n++;
   rep.stick{n} = stick();
@@ -66,14 +64,7 @@ function rep = rep_refaxes(addto="",scale=1,srad=1)
   rep.stick{n}.r = srad * 0.05;
   rep.stick{n}.rgb = [0 255 0 0 0];
   rep.stick{n}.tex = itex;
-  # n++;
-  # rep.stick{n} = stick();
-  # rep.stick{n}.name = "y-axis (negative)";
-  # rep.stick{n}.x0 = xct + [0 0 0];
-  # rep.stick{n}.x1 = xct - [0 1 0] * scale;
-  # rep.stick{n}.r = srad * 0.05;
-  # rep.stick{n}.rgb = [0 28 0 0 0];
-  # rep.stick{n}.tex = itex;
+
   ## z axis
   n++;
   rep.stick{n} = stick();
@@ -83,14 +74,7 @@ function rep = rep_refaxes(addto="",scale=1,srad=1)
   rep.stick{n}.r = srad * 0.05;
   rep.stick{n}.rgb = [0 0 255 0 0];
   rep.stick{n}.tex = itex;
-  # n++;
-  # rep.stick{n} = stick();
-  # rep.stick{n}.name = "z-axis (negative)";
-  # rep.stick{n}.x0 = xct + [0 0 0];
-  # rep.stick{n}.x1 = xct - [0 0 1] * scale;
-  # rep.stick{n}.r = srad * 0.05;
-  # rep.stick{n}.rgb = [0 0 28 0 0];
-  # rep.stick{n}.tex = itex;
+
   rep.nstick = n;
 
 endfunction
