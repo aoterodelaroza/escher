4ca55d3e66b04c67c9093912fc926eccfee4ad71
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2016-06-21 15:48:51 -0700

    updated mol_writeturbo with BSIC stuff

--- src/mol_writeturbo.m
+++ src/mol_writeturbo.m
@@ -10,10 +10,13 @@
 % FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
 % more details.
 
-function err = mol_writeturbo(mol, filename="")
-%function err = mol_writeturbo(mol, filename="")
+function err = mol_writeturbo(mol, filename="", bsic="")
+%function err = mol_writeturbo(mol, filename="", bsic="")
 %
-% mol_writegjf - write a sequence for turbomole's define program
+% mol_writegjf - write a sequence for turbomole's define program.
+%   If filename is not given, use stdout. If bsic is non-empty,
+%   use the tricks for BSIC calculations. Pass the name of the 
+%   basis set file in the bsic keyword (e.g. minis).
 %
 
   err = 1;
@@ -27,23 +30,57 @@ function err = mol_writeturbo(mol, filename="")
      endif
   endif
 
+  ats = cell(mol.nat,1);
+  atz = zeros(mol.nat,1);
+  for i = 1:mol.nat
+    atz(i) = mol_dbatom(mol.atname{i});
+    ats{i} = lower(mol_dbsymbol(atz(i)));
+  endfor
+  uatz = unique(atz);
+  uats = cell(length(uatz),1);
+  for i = 1:length(uatz)
+    uats{i} = lower(mol_dbsymbol(uatz(i)));
+  endfor
+
   fprintf(fid,"\n");
   fprintf(fid,"title\n");
   fprintf(fid,"ai\n");
-  for i = 1 : mol.nat;
-     fprintf(fid,"%s\n", mol.atname{i});
+  for i = 1:mol.nat;
+     fprintf(fid,"%s\n", ats{i});
      fprintf(fid,"%15.9f %15.9f %15.9f A\n", mol.atxyz(1:3,i));
      fprintf(fid,"*\n");
   endfor
   fprintf(fid,"*\n");
   fprintf(fid,"*\n");
   fprintf(fid,"no\n");
-  fprintf(fid,"bb all aug-cc-pVTZ\n");
+  if (!isempty(bsic))
+    for i = 1:length(uats)
+      fprintf(fid,"b \"%s\" %s\n",uats{i},bsic);
+      fprintf(fid,"file %s.tbs\n",bsic);
+    endfor
+  else
+    fprintf(fid,"bb all aug-cc-pVTZ\n");
+  endif
   fprintf(fid,"*\n");
   fprintf(fid,"eht\n");
   fprintf(fid,"y\n");
   fprintf(fid,"0\n");
   fprintf(fid,"y\n");
+  if (!isempty(bsic)) 
+    fprintf(fid,"&\n");
+    fprintf(fid,"&\n");
+    fprintf(fid,"y\n");
+    for i = 1:length(uats)
+      fprintf(fid,"c \"%s\" %d\n",uats{i},uatz(i)+2);
+    endfor
+    for i = 1:length(uats)
+      fprintf(fid,"ecp \"%s\" %s-bsic\n",uats{i},bsic);
+      fprintf(fid,"file %s.tbsic\n",bsic);
+    endfor
+    fprintf(fid,"*\n");
+    fprintf(fid,"*\n");
+    fprintf(fid,"\n");
+  endif
   fprintf(fid,"dft\n");
   fprintf(fid,"func\n");
   fprintf(fid,"b-lyp\n");
@@ -52,7 +89,7 @@ function err = mol_writeturbo(mol, filename="")
   fprintf(fid,"on\n");
   fprintf(fid,"*\n");
   fprintf(fid,"ri\n");
-  fprintf(fid,"on\n");
+  fprintf(fid,"off\n");
   fprintf(fid,"*\n");
   fprintf(fid,"*\n");
 
