b940be43b02f0de03b11902b841e55aa22f45666
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2016-06-21 12:39:13 -0700

    implemented headers in mol_writezmat

--- src/mol_writezmat.m
+++ src/mol_writezmat.m
@@ -10,8 +10,8 @@
 % FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
 % more details.
 
-function mol_writezmat(mol,file="",izmat=[],isvar=[])
-% function zmat = mol_writezmat(mol,file="",izmat=[],isvar=[])
+function mol_writezmat(mol,file="",header=0,izmat=[],izvar=[])
+% function zmat = mol_writezmat(mol,file="",header=0,izmat=[],izvar=[])
 %
 % mol_writezmat - write a zmatrix calculated from the Cartesian
 %   coordinates in mol. If no izmat is present, the atoms are taken in the
@@ -26,23 +26,28 @@ function mol_writezmat(mol,file="",izmat=[],isvar=[])
 %   (dihedral). The 2:4 entries in the first line, 3:4 in the second line, and 
 %   fourth entry in the third line are ignored.
 %
-%   If isvar is present, make some of the entries in the z-matrix "variables"
-%   in Gaussian's input format. isvar should have the same structure as izmat.
-%   If an entry in isvar is non-zero, it is made a variable.
+%   If izvar is present, make some of the entries in the z-matrix "variables"
+%   in Gaussian's input format. izvar should have the same structure as izmat.
+%   If an entry in izvar is non-zero, it is made a variable.
 % 
 % Required input variables:
 % mol: the input molecule
+%
+% Optional input variables:
 % file: output file (zmat). If not present, write to standard output.
+% header: if true (1), write a proper Gaussian input file.
 % izmat: array of atomic indices for the z-matrix construction. 
 % If not present, use the atomic sequence in input order.
 %
 
+  ## open the output file
   if (!isempty(file))
     fid = fopen(file,"w");
   else
     fid = stdout();
   endif
 
+  ## fill the izmat
   if (isempty(izmat)) 
     izmat = zeros(mol.nat,4);
     for i = 1:mol.nat
@@ -54,15 +59,35 @@ function mol_writezmat(mol,file="",izmat=[],isvar=[])
     izmat(3,4) = 0;
   endif
   
+  ## write the header, if applicable
+  if (header)
+    nelec = sum(mol.atnumber);
+    if (mod(nelec,2) == 0)
+      mult = 1;
+    else
+      mult = 2;
+    endif
+    fprintf(fid, "%%mem=2GB\n");
+    fprintf(fid, "%%nproc=4\n");
+    fprintf(fid, "#p b3lyp sto-3g\n");
+    fprintf(fid, "\n");
+    fprintf(fid, "title\n");
+    fprintf(fid, "\n");
+    fprintf(fid, "0 %d\n",mult);
+  endif
+
+  ## print the body of the z-matrix
   nvars = 0;
   svars = {};
   dvars = [];
   for i = 1:rows(izmat)
-    fprintf(fid,"%s ",mol.atname{izmat(i,1)});
+    ii0 = izmat(i,1);
+    fprintf(fid,"%s ",mol.atname{ii0});
     if (izmat(i,2) > 0)
       i1 = izmat(i,2);
-      a = op_dist(mol.atxyz(:,i1),mol.atxyz(:,i));
-      if (isvar(i,2))
+      ii1 = izmat(i1,1);
+      a = op_dist(mol.atxyz(:,ii1),mol.atxyz(:,ii0));
+      if (!isempty(izvar) && izvar(i,2))
         nvars++;
         fprintf(fid,"%d b%d ",i1,nvars);
         svars{nvars} = sprintf("b%d",nvars);
@@ -73,8 +98,9 @@ function mol_writezmat(mol,file="",izmat=[],isvar=[])
     endif
     if (izmat(i,3) > 0)
       i2 = izmat(i,3);
-      a = op_angle(mol.atxyz(:,i2),mol.atxyz(:,i1),mol.atxyz(:,i));
-      if (isvar(i,3))
+      ii2 = izmat(i2,1);
+      a = op_angle(mol.atxyz(:,ii2),mol.atxyz(:,ii1),mol.atxyz(:,ii0));
+      if (!isempty(izvar) && izvar(i,3))
         nvars++;
         fprintf(fid,"%d a%d ",i2,nvars);
         svars{nvars} = sprintf("a%d",nvars);
@@ -85,8 +111,9 @@ function mol_writezmat(mol,file="",izmat=[],isvar=[])
     endif
     if (izmat(i,4) > 0)
       i3 = izmat(i,4);
-      a = op_dihedral(mol.atxyz(:,i3),mol.atxyz(:,i2),mol.atxyz(:,i1),mol.atxyz(:,i));
-      if (isvar(i,3))
+      ii3 = izmat(i3,1);
+      a = op_dihedral(mol.atxyz(:,ii3),mol.atxyz(:,ii2),mol.atxyz(:,ii1),mol.atxyz(:,ii0));
+      if (!isempty(izvar) && izvar(i,4))
         nvars++;
         fprintf(fid,"%d d%d ",i3,nvars);
         svars{nvars} = sprintf("d%d",nvars);
@@ -99,11 +126,13 @@ function mol_writezmat(mol,file="",izmat=[],isvar=[])
   endfor
   fprintf(fid,"\n");
 
+  ## print the variables
   for i = 1:nvars
     fprintf(fid,"%s %.10f\n",svars{i},dvars(i));
   endfor
   fprintf(fid,"\n");
 
+  ## close the file
   if (!isempty(file))
     fclose(fid);
   endif
