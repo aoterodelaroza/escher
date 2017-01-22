03130511d54e112f74a4724d7bec49afdca08592
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2017-01-22 11:21:06 -0800

    improved mol_ball - read multiple atoms in a cell array

--- src/mol_ball.m
+++ src/mol_ball.m
@@ -17,19 +17,21 @@ function rep = mol_ball(mol,addto="",symb=".+",strict=0,radius=-0.6,rgb=[-1 -1 -
 %
 % Required input variables:
 % mol: structure with the molecular description.
-% symb: atomic symbol. By default, consider all atom types. If strict = 1, 
-%       a regular expression can be used (see below).
+% symb: atomic symbol or cell array of atomic symbols to represent. By
+%   default, consider all atom types. If strict = 1, a regular
+%   expression can be used (see below). 
 %
 % Optional input variables (all have default values):
 % addto: input representation on which the new representation is 
 %        added.
-% strict: if false (0), then symb is converted to an atomic number and 
-%         balls are represented for all atoms in mol with the same Z. If strict 
-%         is true (1), then only atoms matching that name are represented. Octave
-%         regular expressions (compared to the atomic names using regexp)
-%         can be used in this case.
-%         strict=2 is the same as 1, but use symb as a strings (compare with strcmp), 
-%         not a regular expression.
+% strict: if zero (0), then symb is converted to an atomic number and
+%         balls are represented for all atoms in mol with the same
+%         Z. If strict is one (1), then only atoms matching that
+%         string are represented. Atomic names or octave regular
+%         expressions (compared to the atomic names in mol using
+%         regexp) can be used in this case. strict=2 is the same as 1,
+%         but use symb as a strings (compare with strcmpi), not a
+%         regular expression (case insensitive). Default: 0.
 % radius: the ball radius in angstrom. If a negative number is given,
 %         the ball radius is taken as abs(radius) times the covalent radius.
 % rgb: the ball color. By default (or if any of the rgb components is negative, use 
@@ -55,42 +57,53 @@ function rep = mol_ball(mol,addto="",symb=".+",strict=0,radius=-0.6,rgb=[-1 -1 -
     endif
   endif
 
-  ## Create balls
-  if (strict != 1 && strict != 2)
-    if (strcmpi(symb,".+"))
-      zz = -1;
-    else
-      zz = mol_dbatom(symb);
-    endif
+  ## Mark the atoms to represent
+  if (!iscell(symb))
+    symb = {symb};
   endif
+  doit = zeros(1,nat);
+  for j = 1:length(symb)
+    if (strict != 1 && strict != 2)
+      if (strcmpi(symb{j},".+"))
+        zz = -1;
+      else
+        zz = mol_dbatom(symb{j});
+      endif
+    endif
+    for i = 1:nat
+      if (strict == 2)
+        doit(i) = doit(i) || strcmpi(mol.atname{i},symb{j});
+      elseif (strict == 1)
+        doit(i) = doit(i) || regexp(tolower(mol.atname{i}),tolower(symb{j}));
+      else
+        doit(i) = doit(i) || ((zz < 0) || (zz == mol.atnumber(i)));
+      endif
+    endfor
+  endfor
+
+  ## Create balls
   [rep itex] = rep_registertexture(rep,tex);
   for i = 1:nat
-    if (strict == 2)
-      doit = strcmpi(mol.atname{i},symb);
-    elseif (strict == 1)
-      doit = regexp(tolower(mol.atname{i}),tolower(symb));
+    if (!doit(i)) 
+      continue
+    endif
+    n = rep.nball = rep.nball+1;
+    rep.ball{n} = ball();
+    rep.ball{n}.x = mol.atxyz(:,i)';
+    rep.ball{n}.name = mol.atname{i};
+    [dum, atom] = mol_dbsymbol(mol.atnumber(i));
+    if (radius>0) 
+      rep.ball{n}.r = radius;
     else
-      doit = (zz < 0) || (zz == mol.atnumber(i));
+      rep.ball{n}.r = atom.rcov * abs(radius); 
     endif
-    if (doit) 
-      n = rep.nball = rep.nball+1;
-      rep.ball{n} = ball();
-      rep.ball{n}.x = mol.atxyz(:,i)';
-      rep.ball{n}.name = mol.atname{i};
-      [dum, atom] = mol_dbsymbol(mol.atnumber(i));
-      if (radius>0) 
-        rep.ball{n}.r = radius;
-      else
-        rep.ball{n}.r = atom.rcov * abs(radius); 
-      endif
-      if (all(rgb>=0))
-        rep.ball{n}.rgb = rgb;
-      else
-        rep.ball{n}.rgb = [atom.color 0 0];
-      endif
-      rep.ball{n}.tex = itex;
-      rep.ball{n}.wire = wire;
+    if (all(rgb>=0))
+      rep.ball{n}.rgb = rgb;
+    else
+      rep.ball{n}.rgb = [atom.color 0 0];
     endif
+    rep.ball{n}.tex = itex;
+    rep.ball{n}.wire = wire;
   endfor
 
   ## register shapes3.inc for loading if using rounded cylinders
